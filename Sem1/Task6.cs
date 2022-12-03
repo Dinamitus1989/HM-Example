b4fa215bb68ba881f5bc6279f027a6c0dcb70f0b
slava <slafa@tut.by>
2022-12-03 12:25:45 +0300

    третья задача

--- Sem1/Task6.cs
+++ Sem1/Task6.cs
@@ -1,1 +1,15 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите число");
+string A = Console.ReadLine()!;
+
+int ParsedInputA = int.Parse(A);
+
+
+if (ParsedInputA % 2 == 0)
+
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
