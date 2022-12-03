da2430478a9adab8db8d5016c110f02600de0674
slava <slafa@tut.by>
2022-12-03 12:26:31 +0300

    четвертая задача

--- Sem1/Task8.cs
+++ Sem1/Task8.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите число");
+string A = Console.ReadLine()!;
+
+int ParsedInputA = int.Parse(A);
+
+for (int i = 0; i <= ParsedInputA; i++)
+{
+    if (i % 2 == 0) 
+    Console.WriteLine(i);
+
+}
