5acef9c8b45a8856495312ee953d544cb5a98807
slava <slafa@tut.by>
2022-12-03 12:23:08 +0300

    первая задача

--- Sem1/Task2.cs
+++ Sem1/Task2.cs
@@ -1,1 +1,18 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите числа");
+string A = Console.ReadLine()!;
+string B = Console.ReadLine()!;
+int ParsedInputA = int.Parse(A);
+int ParsedInputB = int.Parse(B);
+
+if(ParsedInputA > ParsedInputB)
+{
+    Console.Write("max=");
+    Console.WriteLine(A);
+}
+
+else
+{
+    Console.Write("max=");
+    Console.Write(B);
+
+}
