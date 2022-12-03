870bde2413773723ae4337105f907fde9cf70aef
slava <slafa@tut.by>
2022-12-03 12:24:48 +0300

    вторая задача

--- Sem1/Task4.cs
+++ Sem1/Task4.cs
@@ -1,1 +1,13 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите числа");
+string A = Console.ReadLine()!;
+string B = Console.ReadLine()!;
+string C = Console.ReadLine()!;
+int ParsedInputA = int.Parse(A);
+int ParsedInputB = int.Parse(B);
+int ParsedInputC = int.Parse(C);
+int max = ParsedInputA;
+if (ParsedInputA > max) max = ParsedInputA;
+if (ParsedInputB > max) max = ParsedInputB;
+if (ParsedInputC > max) max = ParsedInputC;
+
+Console.WriteLine(max);
