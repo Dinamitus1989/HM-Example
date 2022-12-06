fd331ffb2c00f29f0699ebbe24ddf1f2ed1eae12
slava <slafa@tut.by>
2022-12-06 11:49:35 +0300

    задача 10

--- Sem2/Task10.cs
+++ Sem2/Task10.cs
@@ -1,1 +1,7 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = number / 10;
+int SecondDigit = result % 10;
+
+Console.WriteLine(SecondDigit);
