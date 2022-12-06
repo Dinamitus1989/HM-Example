dbd6add581968b8d306a212baed6111038d79bd2
slava <slafa@tut.by>
2022-12-06 15:59:31 +0300

    задача 10

--- Sem2/Task10.cs
+++ Sem2/Task10.cs
@@ -2,11 +2,12 @@ Console.WriteLine("Введите трехзначное число:");
 
 
 int number = int.Parse(Console.ReadLine()!);
-if (number < 1000)
-{
+if (((number > 99) && (number < 1000)) || ((number < -99) && (number > -1000)))
+ {
+    
     int result = number / 10;
     int SecondDigit = result % 10;
-
+    
     Console.WriteLine(SecondDigit);
 }
 
