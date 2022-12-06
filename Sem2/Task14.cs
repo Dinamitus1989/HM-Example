abb8c9d674fa0dfcb4ca2a3300e07b8de1d05ce9
slava <slafa@tut.by>
2022-12-06 15:57:46 +0300

    задача 10

---
+++ Sem2/Task14.cs
@@ -0,0 +1,13 @@
+int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
