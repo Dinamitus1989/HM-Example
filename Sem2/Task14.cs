85d115193336c5308655fbea7be9c1ed87831187
slava <slafa@tut.by>
2022-12-06 16:01:27 +0300

    задача 14

--- Sem2/Task14.cs
+++ Sem2/Task14.cs
@@ -1,13 +1,18 @@
-int ThirdDigit(int number)
-        {
-            int result = -1;
-            if (number >= 100)
-            {
-                while (number > 999)
-                {
-                    number = number / 10;
-                }
-                result = number % 10;
-            }
-            return result; 
-        }
+Console.WriteLine("Введите число:");
+
+int number = int.Parse(Console.ReadLine()!);
+
+if (number <= 0 || number > 7)
+{
+    Console.WriteLine("неверный ввод");
+}
+
+else if (number > 5 && number < 8)
+
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
