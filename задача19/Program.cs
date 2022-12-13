badc9e047c3169eacb3a8665cd6dc899293f0343
slava <slafa@tut.by>
2022-12-13 15:23:00 +0300

    задача19

---
+++ задача19/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+
+if( 10000 <= number && number <= 99999 )
+{
+    int Digit1 = number / 10000;
+    int Digit2 = number / 1000 % 10;
+    int Digit4 = number / 10 % 10;
+    int Digit5 = number % 10;
+    
+
+    if(Digit1 == Digit5 && Digit2 == Digit4)
+
+    {
+        Console.WriteLine("Да.Число является палиндромом");
+
+    }
+
+    else
+    {
+        Console.WriteLine("Нет.Число не является палиндромом");
+    }
+
+}else
+{
+    Console.WriteLine("Неверный ввод.Введите пятизначное число");
+}
