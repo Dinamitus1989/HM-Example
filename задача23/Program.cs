3579edba0f1b4fe874011867ade4d96b4a9c0043
slava <slafa@tut.by>
2022-12-13 15:25:37 +0300

    задача23

---
+++ задача23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 0)
+{
+    number = Math.Abs(number);
+}
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
