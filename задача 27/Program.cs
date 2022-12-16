3d671689129c896bbc36566bf78abd642449ac76
slava <slafa@tut.by>
2022-12-16 18:21:49 +0300

    27

---
+++ задача 27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine(SumDigits(number1));
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+       
+    }
+    return sum;
+}
