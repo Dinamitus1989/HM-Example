bb268aaab8b8741182e27030f2efd81bd5b6ea57
slava <slafa@tut.by>
2022-12-16 18:08:43 +0300

    25

---
+++ задача 25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите два числа");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+int result = Power(number1, number2);
+Console.WriteLine(result);
+
+int Power(int a, int b)
+{
+    int mult = a;
+    for (int i = 1; i < b; i++)
+    {
+        mult = mult * a;
+    }
+    return mult;
+}
