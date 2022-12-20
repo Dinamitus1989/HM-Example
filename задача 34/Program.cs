e1cd8427f29d640bf79a5e37386f51b2b77b562d
slava <slafa@tut.by>
2022-12-20 17:29:54 +0300

    34

---
+++ задача 34/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите размер массива");
+int number1 = int.Parse(Console.ReadLine()!);
+
+int[] firstArray = GenerateArray(number1);
+PrintArray(firstArray);
+
+Console.WriteLine(CountEven(firstArray));
+
+int[] GenerateArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
