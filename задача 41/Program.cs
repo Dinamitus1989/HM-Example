df148d66cd2479ade8f217ac6268118f2754870d
slava <slafa@tut.by>
2022-12-23 19:03:39 +0300

    41

---
+++ задача 41/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите количество чисел");
+int numberCount = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(CountPositiveNumbers(numberCount));
+
+int CountPositiveNumbers(int numberCount)
+{
+    int counter = 0;
+    for (int i = 0; i < numberCount; i++)
+    {
+        int CurrentNumber = int.Parse(Console.ReadLine()!);
+
+        if (IsPositiveNumbers(CurrentNumber))
+        {
+            counter++;
+        }
+
+    }
+    return counter;
+}
+
+bool IsPositiveNumbers(int number)
+{
+    return number > 0;
+
+}
