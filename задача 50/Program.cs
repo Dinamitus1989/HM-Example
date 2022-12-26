6d41607ffd1e3aa417e4a126b4eab6b587207f0f
slava <slafa@tut.by>
2022-12-26 21:29:38 +0300

    47

---
+++ задача 50/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+double[,] mainMatrix = Create2DArray(m, n, -5, 10);
+Print2DArray(mainMatrix);
+
+double[,] Create2DArray(int m, int n, int min, int max)
+{
+    double[,] matrix = new double[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((max - min) * new Random().NextDouble() + min, 2);
+        }
+    }
+    return matrix;
+}
+
+
+
+void Print2DArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
